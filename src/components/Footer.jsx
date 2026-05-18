fb047d84667fd09371fd03138c0ae3e122823bc5
Mostafa205dev <seso63412@gmail.com>
2026-05-19 00:21:13 +0200

    fix footer 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,11 @@
 import "./Footer.css";
-import { FaWhatsapp, FaPhone ,FaLinkedin ,FaFacebook ,FaGithub} from "react-icons/fa";
+import {
+  FaWhatsapp,
+  FaPhone,
+  FaLinkedin,
+  FaFacebook,
+  FaGithub,
+} from "react-icons/fa";
 
 export default function Footer() {
   return (
@@ -14,17 +20,27 @@ export default function Footer() {
           <FaWhatsapp size={30} color="green" />
           01155410622
         </p>
-        <a href="https://www.linkedin.com/in/mostafa-tarek-448665250/" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.linkedin.com/in/mostafa-tarek-448665250/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaLinkedin size={25} color="#0A66C2" />
         </a>
-        <a href="https://www.facebook.com/mostafa.tarek.678889" target="_blank" rel="noreferrer">
+        <a
+          href="https://www.facebook.com/mostafa.tarek.678889"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaFacebook size={25} color="#0A66C2" />
         </a>
-        <a href="https://github.com/Mostafa205dev" target="_blank" rel="noreferrer">
+        <a
+          href="https://github.com/Mostafa205dev"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FaGithub size={25} color="#0A66C2" />
         </a>
-        
-        <p>Careers</p>
       </div>
       <h3 className="rights">Mostafa Tarek Hassanien</h3>
     </div>
