c192f3fe7244865c682fbc6ec80a9fc79a22f745
Mostafa205dev <seso63412@gmail.com>
2026-05-18 21:14:27 +0200

    added personal details in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,32 @@
 import "./Footer.css";
+import { FaWhatsapp, FaPhone ,FaLinkedin ,FaFacebook ,FaGithub} from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className="footer">
       <h3 className="icon">NekhrogFeen</h3>
       <div className="links">
-        <p>Privcy</p>
-        <p>Terms</p>
-        <p>Support</p>
+        <p>
+          <FaPhone />
+          01023113960
+        </p>
+        <p>
+          <FaWhatsapp size={30} color="green" />
+          01155410622
+        </p>
+        <a href="https://www.linkedin.com/in/mostafa-tarek-448665250/" target="_blank" rel="noreferrer">
+          <FaLinkedin size={25} color="#0A66C2" />
+        </a>
+        <a href="https://www.facebook.com/mostafa.tarek.678889" target="_blank" rel="noreferrer">
+          <FaFacebook size={25} color="#0A66C2" />
+        </a>
+        <a href="https://github.com/Mostafa205dev" target="_blank" rel="noreferrer">
+          <FaGithub size={25} color="#0A66C2" />
+        </a>
+        
         <p>Careers</p>
       </div>
-      <h3 className="rights">© 2024 . All rights reserved.</h3>
+      <h3 className="rights">Mostafa Tarek Hassanien</h3>
     </div>
   );
 }
